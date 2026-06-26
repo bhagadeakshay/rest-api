6aa3d61903ded6d591fd25ac4b49fc932121f64c
bhagadeakshay <bhagadeakshay2@gmail.com>
2026-06-26 11:02:42 +0530

    backend prepate

--- todo-backend/app.js
+++ todo-backend/app.js
@@ -1,34 +1,35 @@
 const express = require("express");
-const path = require("path");
-const { default: mongoose } = require("mongoose");
+const mongoose = require("mongoose");
 const cors = require("cors");
-
-const DB_PATH =
-  "mongodb+srv://Akshay:akshay@shophurtz.fu4wqds.mongodb.net/shopHurtz?appName=shopHurtz";
 
 const todoItemsRouter = require("./routes/todoItemsRouter");
 const errorsController = require("./controllers/errors");
 
 const app = express();
 
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
+
+app.use(
+  cors({
+    origin: "*",
+  }),
+);
+
 app.use("/api/todo", todoItemsRouter);
 
 app.use(errorsController.pageNotFound);
 
-const PORT = 3000;
+// Read MongoDB URL from environment variable
+const DB_PATH = process.env.MONGO_URI;
 
 mongoose
   .connect(DB_PATH)
   .then(() => {
-    console.log("Connected to database");
-    app.listen(PORT, () => {
-      console.log(`Server is running on address
-     http://localhost:${PORT}`);
-    });
+    console.log("Connected to MongoDB");
   })
   .catch((err) => {
-    console.log("error while connecting to database", err);
+    console.log(err);
   });
+
+module.exports = app;
